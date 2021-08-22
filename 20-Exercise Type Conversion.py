f42117b0012dc08655978c0274f3283528512005
JaK0B-Pro <87572423+JaK0B-Pro@users.noreply.github.com>
2021-08-22 13:39:38 +0100

    A simple program that calculate your age
    Going on, this is the first program that i wrote on my journey to learn programming, i hope that you liked it, i should upload this first but the idea hit me when i complete the second program which is a password checker that i must share my information with the world, thank's for giving the time to read this.

---
+++ 20-Exercise Type Conversion.py
@@ -0,0 +1,8 @@
+# 20-Exercise Type Conversion:
+
+birth_year = int(input('What year were you born? \n I have born in: '))
+the_year = 2021
+
+age = the_year - birth_year
+
+print(f'Hurray, Your age is {age} years old, GOD BLESS YOU.')
